4fdf969ee6dc135b5231928271ad92b2e4ebe4c0
KhatriSuson <susonkhatri110@gmail.com>
2024-04-01 15:00:12 +0545

    Initial Commit

--- blogs/boards/models.py
+++ blogs/boards/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=230, unique=True)
+    description = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name = 'topics', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name = 'topics', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=40000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(null= True)
+    create_by = models.ForeignKey(User, related_name ='posts', on_delete = models.CASCADE)
+    update_by = models.ForeignKey(User, null = True, related_name = '+', on_delete=models.CASCADE)
--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Board
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    boards = Board.objects.all()
+    # boards_names = []
+
+    # for board in boards:
+    #     boards_names.append(board.name)
+
+    # response_html = '<br>'.join(boards_names)
+
+    # return HttpResponse(response_html)
+
+    return render(request, 'home.html', {'boards': boards})   
+    
