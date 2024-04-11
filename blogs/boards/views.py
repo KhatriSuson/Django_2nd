ea83a05a5af56248f2363de790e8bb43068df423
KhatriSuson <susonkhatri110@gmail.com>
2024-04-11 09:13:06 +0545

    Initial Commit

--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Board
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 from django.http import HttpResponse, Http404
 
@@ -53,8 +54,19 @@ def year_archive(request, year):
     return HttpResponse(f"Year : {year}")
 
 
-
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'new_topic.html', {'board': board})
+    if request.method == "POST":
+        subject = request.POST["subject"]
+        message = request.POST["message"]
 
+        user = User.objects.first()  #     TODO: GET the currently logged in user
+
+        topic = Topic.objects.create(subject=subject, board=board, starter=user)
+
+        post = Post.objects.create(message=message, topic=topic, create_by=user)
+        return redirect(
+            "board_topics", pk=board.pk
+        )  # TODO:  redirect to the created topic page
+
+    return render(request, "new_topic.html", {"board": board})
